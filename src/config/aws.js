7e1d49abe3d46df7a1cc9d6c021e536987b81f49
Nexus0831 <pride0831@gmail.com>
2017-12-29 15:19:08 +0900

    signup機能（仮）実装

---
+++ src/config/aws.js
@@ -0,0 +1,6 @@
+export const AWS_REGION = 'ap-northeast-1';
+export const AWS_ACCESS_KEY = '';
+export const AWS_SECRET_ACCESS_KEY = '';
+export const AWS_COGNITO_USER_POOL_ID = '';
+export const AWS_COGNITO_APP_CLIENT_ID = '';
+export const AWS_COGNITO_IDENTITY_POOL_ID = '';
