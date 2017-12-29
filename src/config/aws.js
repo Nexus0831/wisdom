63da8442a2ecc528dc89dd9f5a3010e53cbe893e
Nexus0831 <pride0831@gmail.com>
2017-12-29 15:08:13 +0900

    signup機能（仮）実装

--- src/config/aws.js
+++ src/config/aws.js
@@ -1,6 +1,6 @@
 export const AWS_REGION = 'ap-northeast-1';
 export const AWS_ACCESS_KEY = '';
 export const AWS_SECRET_ACCESS_KEY = '';
-export const AWS_COGNITO_USER_POOL_ID = 'ap-northeast-1_ST1wkqu4O';
-export const AWS_COGNITO_APP_CLIENT_ID = '7ocm9estvqp9oiff9e1sh6kjjs';
-export const AWS_COGNITO_IDENTITY_POOL_ID = 'ap-northeast-1:edc192a6-2521-4f6a-b6aa-de8bcfb979e2';
+export const AWS_COGNITO_USER_POOL_ID = '';
+export const AWS_COGNITO_APP_CLIENT_ID = '';
+export const AWS_COGNITO_IDENTITY_POOL_ID = '';
